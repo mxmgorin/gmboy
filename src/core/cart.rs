bb9d1c34cd941a053a204b96bf8a9a9f537750a8
Maxim Gorin <onlinexmax@gmail.com>
2025-01-29 18:23:33 +0300

    Add enum for CartridgeType

--- src/core/cart.rs
+++ src/core/cart.rs
@@ -18,9 +18,9 @@ struct CartHeader {
     pub title: String,           // 0x0134-0x0143: Game title
     pub manufacturer_code: Option<String>, // 0x013F-0x0142: Manufacturer code (if exists)
     pub cgb_flag: u8,            // 0x0143: Game Boy Color compatibility
-    pub new_licensee_code: NewLicenseeCode,  // 0x0144-0x0145: New licensee code
+    pub new_licensee_code: NewLicenseeCode, // 0x0144-0x0145: New licensee code
     pub sgb_flag: u8,            // 0x0146: Super Game Boy compatibility
-    pub cartridge_type: u8,      // 0x0147: Type of cartridge
+    pub cartridge_type: CartridgeType, // 0x0147: Type of cartridge
     pub rom_size: u8,            // 0x0148: ROM size
     pub ram_size: u8,            // 0x0149: RAM size
     pub destination_code: u8,    // 0x014A: Destination code (Japan or non-Japan)
@@ -48,9 +48,11 @@ impl CartHeader {
                 None
             },
             cgb_flag: data[0x0143],
-            new_licensee_code: NewLicenseeCode::try_from_u16(u16::from_be_bytes(data[0x0144..0x0146].try_into().unwrap()))?,
+            new_licensee_code: NewLicenseeCode::try_from_u16(u16::from_be_bytes(
+                data[0x0144..0x0146].try_into().unwrap(),
+            ))?,
             sgb_flag: data[0x0146],
-            cartridge_type: data[0x0147],
+            cartridge_type: data[0x0147].try_into()?,
             rom_size: data[0x0148],
             ram_size: data[0x0149],
             destination_code: data[0x014A],
@@ -81,6 +83,73 @@ impl NewLicenseeCode {
             0x02 => Ok(NewLicenseeCode::Konami),
             0x03 => Ok(NewLicenseeCode::ElectronicArts),
             _ => Err("Unknown licensee_code".into()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum CartridgeType {
+    RomOnly = 0x00,
+    Mbc1 = 0x01,
+    Mbc1Ram = 0x02,
+    Mbc1RamBattery = 0x03,
+    Mbc2 = 0x05,
+    Mbc2Battery = 0x06,
+    RomRam = 0x08,
+    RomRamBattery = 0x09,
+    Mmm01 = 0x0B,
+    Mmm01Ram = 0x0C,
+    Mmm01RamBattery = 0x0D,
+    Mbc3TimerBattery = 0x0F,
+    Mbc3TimerRamBattery = 0x10,
+    Mbc3 = 0x11,
+    Mbc3Ram = 0x12,
+    Mbc3RamBattery = 0x13,
+    Mbc5 = 0x19,
+    Mbc5Ram = 0x1A,
+    Mbc5RamBattery = 0x1B,
+    Mbc5Rumble = 0x1C,
+    Mbc5RumbleRam = 0x1D,
+    Mbc5RumbleRamBattery = 0x1E,
+    PocketCamera = 0xFC,
+    BandaiTama5 = 0xFD,
+    HuC3 = 0xFE,
+    HuC1RamBattery = 0xFF,
+}
+
+impl TryFrom<u8> for CartridgeType {
+    type Error = String;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(CartridgeType::RomOnly),
+            0x01 => Ok(CartridgeType::Mbc1),
+            0x02 => Ok(CartridgeType::Mbc1Ram),
+            0x03 => Ok(CartridgeType::Mbc1RamBattery),
+            0x05 => Ok(CartridgeType::Mbc2),
+            0x06 => Ok(CartridgeType::Mbc2Battery),
+            0x08 => Ok(CartridgeType::RomRam),
+            0x09 => Ok(CartridgeType::RomRamBattery),
+            0x0B => Ok(CartridgeType::Mmm01),
+            0x0C => Ok(CartridgeType::Mmm01Ram),
+            0x0D => Ok(CartridgeType::Mmm01RamBattery),
+            0x0F => Ok(CartridgeType::Mbc3TimerBattery),
+            0x10 => Ok(CartridgeType::Mbc3TimerRamBattery),
+            0x11 => Ok(CartridgeType::Mbc3),
+            0x12 => Ok(CartridgeType::Mbc3Ram),
+            0x13 => Ok(CartridgeType::Mbc3RamBattery),
+            0x19 => Ok(CartridgeType::Mbc5),
+            0x1A => Ok(CartridgeType::Mbc5Ram),
+            0x1B => Ok(CartridgeType::Mbc5RamBattery),
+            0x1C => Ok(CartridgeType::Mbc5Rumble),
+            0x1D => Ok(CartridgeType::Mbc5RumbleRam),
+            0x1E => Ok(CartridgeType::Mbc5RumbleRamBattery),
+            0xFC => Ok(CartridgeType::PocketCamera),
+            0xFD => Ok(CartridgeType::BandaiTama5),
+            0xFE => Ok(CartridgeType::HuC3),
+            0xFF => Ok(CartridgeType::HuC1RamBattery),
+            _ => Err(format!("Unknown CartridgeType: {value}"))
         }
     }
 }
