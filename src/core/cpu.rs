5323eabd7121925b2f351b8e223107dcc579a2f2
Maxim Gorin <onlinexmax@gmail.com>
2025-01-30 18:02:10 +0300

    Partial impl cpu step

--- src/core/cpu.rs
+++ src/core/cpu.rs
@@ -4,6 +4,9 @@ use crate::core::bus::Bus;
 pub struct Cpu {
     bus: Bus,
     registers: CpuRegisters,
+    current_opcode: u8,
+    current_instruction: Option<&'static CpuInstruction>,
+    halted: bool,
 }
 
 impl Cpu {
@@ -11,22 +14,78 @@ impl Cpu {
         Self {
             bus,
             registers: CpuRegisters::new(),
+            current_opcode: 0,
+            current_instruction: None,
+            halted: false,
         }
     }
 
     pub fn step(&mut self) -> Result<(), String> {
+        if self.halted {
+            return Ok(());
+        }
+
+        self.fetch_instruction();
+        //self.fetch_data();
+        self.execute()?;
+
         Err("cpu step not implemented yet".to_string())
+    }
+
+    fn execute(&mut self) -> Result<(), String> {
+        let Some(current_instruction) = self.current_instruction.take() else {
+            return Err(format!(
+                "Unknown instruction opcode: {}",
+                self.current_opcode
+            ));
+        };
+
+        Ok(())
+    }
+
+    fn fetch_instruction(&mut self) {
+        self.current_opcode = self.bus.read(self.registers.pc);
+        self.registers.pc += 1;
+        self.current_instruction = get_instruction_by_opcode(self.current_opcode);
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct CpuInstruction {
-    pub r#type: InstructionType,
-    pub address_mode: AddressMode,
-    pub register_1_type: RegisterType,
-    pub register_2_type: RegisterType,
-    pub condition_type: ConditionType,
-    pub param: u8,
+    pub r#type: Option<InstructionType>,
+    pub address_mode: Option<AddressMode>,
+    pub register_1_type: Option<RegisterType>,
+    pub register_2_type: Option<RegisterType>,
+    pub condition_type: Option<ConditionType>,
+    pub param: Option<u8>,
+}
+
+const CPU_INSTRUCTIONS: [CpuInstruction; 0x100] = {
+    let mut instructions = [CpuInstruction {
+        r#type: None,
+        address_mode: None,
+        register_1_type: None,
+        register_2_type: None,
+        condition_type: None,
+        param: None,
+    }; 0x100];
+
+    instructions[0x00] = CpuInstruction {
+        r#type: Some(InstructionType::Nop),
+        address_mode: Some(AddressMode::Imp),
+        register_1_type: None,
+        register_2_type: None,
+        condition_type: None,
+        param: None,
+    };
+
+    // todo: Add more instructions here...
+
+    instructions
+};
+
+fn get_instruction_by_opcode(opcode: u8) -> Option<&'static CpuInstruction> {
+    CPU_INSTRUCTIONS.get(opcode as usize)
 }
 
 #[derive(Debug, Clone)]
