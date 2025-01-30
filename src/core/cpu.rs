d698ab8c1e8e5d43341d659ca9cb5e7ebb3d6ebb
Maxim Gorin <onlinexmax@gmail.com>
2025-01-30 17:02:40 +0300

    Add cpu instruction

--- src/core/cpu.rs
+++ src/core/cpu.rs
@@ -16,6 +16,16 @@ impl Cpu {
 }
 
 #[derive(Debug, Clone)]
+pub struct CpuInstruction {
+    pub r#type: InstructionType,
+    pub address_mode: AddressMode,
+    pub register_1_type: RegisterType,
+    pub register_2_type: RegisterType,
+    pub condition_type: ConditionType,
+    pub param: u8,
+}
+
+#[derive(Debug, Clone)]
 pub struct CpuRegisters {
     pub a: u8,
     pub f: u8,
@@ -44,4 +54,113 @@ impl CpuRegisters {
             pc: 0x100,
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AddressMode {
+    Imp,
+    R_D16,
+    R_R,
+    MR_R,
+    R,
+    R_D8,
+    R_MR,
+    R_HLI,
+    R_HLD,
+    HLI_R,
+    HLD_R,
+    R_A8,
+    A8_R,
+    HL_SPR,
+    D16,
+    D8,
+    D16_R,
+    MR_D8,
+    MR,
+    A16_R,
+    R_A16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RegisterType {
+    None,
+    A,
+    F,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+    AF,
+    BC,
+    DE,
+    HL,
+    SP,
+    PC,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum InstructionType {
+    None,
+    Nop,
+    Ld,
+    Inc,
+    Dec,
+    Rlca,
+    Add,
+    Rrca,
+    Stop,
+    Rla,
+    Jr,
+    Rra,
+    Daa,
+    Cpl,
+    Scf,
+    Ccf,
+    Halt,
+    Adc,
+    Sub,
+    Sbc,
+    And,
+    Xor,
+    Or,
+    Cp,
+    Pop,
+    Jp,
+    Push,
+    Ret,
+    Cb,
+    Call,
+    Reti,
+    Ldh,
+    Jphl,
+    Di,
+    Ei,
+    Rst,
+    Err,
+    Rlc,
+    Rrc,
+    Rl,
+    Rr,
+    Sla,
+    Sra,
+    Swap,
+    Srl,
+    Bit,
+    Res,
+    Set,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ConditionType {
+    None,
+    /// Non-zero
+    Nz,
+    /// Zero
+    Z,
+    /// Non-carry
+    Nc,
+    /// Carry
+    C,
 }
