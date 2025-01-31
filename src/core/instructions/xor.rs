fee360b84f2e460673dc6939eac828302b412e93
Maxim Gorin <onlinexmax@gmail.com>
2025-01-31 20:06:38 +0300

    Impl xor

--- src/core/instructions/xor.rs
+++ src/core/instructions/xor.rs
@@ -7,8 +7,9 @@ pub struct XorInstruction {
 }
 
 impl ExecutableInstruction for XorInstruction {
-    fn execute(&self, _cpu: &mut Cpu) {
-        eprintln!("LD instruction not implemented");
+    fn execute(&self, cpu: &mut Cpu) {
+        cpu.registers.a ^= (cpu.fetched_data & 0xFF) as u8;
+        cpu.set_flags((cpu.registers.a == 0) as i8, 0, 0, 0);
     }
 
     fn get_address_mode(&self) -> AddressMode {
