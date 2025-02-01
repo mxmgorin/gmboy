2da2bd68877c2512e42f27b93d404202716c5ad4
Maxim Gorin <onlinexmax@gmail.com>
2025-02-01 13:56:44 +0300

    Refactoring

--- src/core/bus.rs
+++ src/core/bus.rs
@@ -7,59 +7,19 @@ pub struct Bus {
     ram: Ram,
 }
 
-const ROM_BANK0: AddressRange = AddressRange {
-    start: 0x0000,
-    end: 0x3FFF,
-};
-const ROM_BANK1: AddressRange = AddressRange {
-    start: 0x4000,
-    end: 0x7FFF,
-};
-const CHR_RAM: AddressRange = AddressRange {
-    start: 0x8000,
-    end: 0x97FF,
-};
-const BG_MAP1: AddressRange = AddressRange {
-    start: 0x9800,
-    end: 0x9BFF,
-};
-const BG_MAP2: AddressRange = AddressRange {
-    start: 0x9C00,
-    end: 0x9FFF,
-};
-
-const CART_RAM: AddressRange = AddressRange {
-    start: 0xA000,
-    end: 0xBFFF,
-};
-const RAM_BANK0: AddressRange = AddressRange {
-    start: 0xC000,
-    end: 0xCFFF,
-};
-const RAM_BANK1TO7: AddressRange = AddressRange {
-    start: 0xD000,
-    end: 0xDFFF,
-};
-const ECHO_RAM: AddressRange = AddressRange {
-    start: 0xE000,
-    end: 0xFDFF,
-};
-const OBJECT_ATTRIBUTE_MEMORY: AddressRange = AddressRange {
-    start: 0xFE00,
-    end: 0xFE9F,
-};
-const UNUSABLE: AddressRange = AddressRange {
-    start: 0xFEA0,
-    end: 0xFEFF,
-};
-const IO_REGISTERS: AddressRange = AddressRange {
-    start: 0xFF00,
-    end: 0xFF7F,
-};
-const ZERO_PAGE: AddressRange = AddressRange {
-    start: 0xFF80,
-    end: 0xFFFE,
-};
+const ROM_BANK0: AddrRange = AddrRange(0x0000, 0x3FFF);
+const ROM_BANK1: AddrRange = AddrRange(0x4000, 0x7FFF);
+const CHR_RAM: AddrRange = AddrRange(0x8000, 0x97FF);
+const BG_MAP1: AddrRange = AddrRange(0x9800, 0x9BFF);
+const BG_MAP2: AddrRange = AddrRange(0x9C00, 0x9FFF);
+const CART_RAM: AddrRange = AddrRange(0xA000, 0xBFFF);
+const RAM_BANK0: AddrRange = AddrRange(0xC000, 0xCFFF);
+const RAM_BANK1TO7: AddrRange = AddrRange(0xD000, 0xDFFF);
+const ECHO_RAM: AddrRange = AddrRange(0xE000, 0xFDFF);
+const OBJECT_ATTRIBUTE_MEMORY: AddrRange = AddrRange(0xFE00, 0xFE9F);
+const UNUSABLE: AddrRange = AddrRange(0xFEA0, 0xFEFF);
+const IO_REGISTERS: AddrRange = AddrRange(0xFF00, 0xFF7F);
+const ZERO_PAGE: AddrRange = AddrRange(0xFF80, 0xFFFE);
 
 impl Bus {
     pub fn new(cart: Cart, ram: Ram) -> Self {
@@ -96,14 +56,11 @@ impl Bus {
 }
 
 #[derive(Debug, Clone)]
-pub struct AddressRange {
-    pub start: u16,
-    pub end: u16,
-}
+pub struct AddrRange(u16, u16);
 
-impl AddressRange {
+impl AddrRange {
     pub fn contains(&self, address: u16) -> bool {
-        self.start <= address && address <= self.end
+        self.0 <= address && address <= self.1
     }
 }
 
@@ -117,6 +74,5 @@ mod tests {
 
         //assert!(RAM_BANK1.contains(0x100));
         //assert!(RAM_BANK0.contains(0x100));
-
     }
 }
