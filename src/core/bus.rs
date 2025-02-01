3402264e947a72ca4843ff905257de0bd52df47a
Maxim Gorin <onlinexmax@gmail.com>
2025-02-01 14:33:39 +0300

    Impl bus read, write for more cases

--- src/core/bus.rs
+++ src/core/bus.rs
@@ -5,6 +5,7 @@ use crate::core::ram::Ram;
 pub struct Bus {
     cart: Cart,
     ram: Ram,
+    ie_register: u8,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,83 +22,82 @@ pub enum AddrLocation {
     ObjectAttributeMemory, // 0xFE00 - 0xFE9F
     Unusable,              // 0xFEA0 - 0xFEFF
     IoRegisters,           // 0xFF00 - 0xFF7F
-    ZeroPage,              // 0xFF80 - 0xFFFE
+    /// Aka High RAM (HRAM)
+    ZeroPage, // 0xFF80 - 0xFFFE
+    IeRegister,            // 0xFFFF
 }
 
-impl TryFrom<u16> for AddrLocation {
-    type Error = String;
-
-    fn try_from(address: u16) -> Result<Self, Self::Error> {
+impl From<u16> for AddrLocation {
+    fn from(address: u16) -> Self {
         match address {
-            0x0000..=0x3FFF => Ok(AddrLocation::RomBank0),
-            0x4000..=0x7FFF => Ok(AddrLocation::RomBank1),
-            0x8000..=0x97FF => Ok(AddrLocation::ChrRam),
-            0x9800..=0x9BFF => Ok(AddrLocation::BgMap1),
-            0x9C00..=0x9FFF => Ok(AddrLocation::BgMap2),
-            0xA000..=0xBFFF => Ok(AddrLocation::CartRam),
-            0xC000..=0xCFFF => Ok(AddrLocation::RamBank0),
-            0xD000..=0xDFFF => Ok(AddrLocation::RamBank1To7),
-            0xE000..=0xFDFF => Ok(AddrLocation::EchoRam),
-            0xFE00..=0xFE9F => Ok(AddrLocation::ObjectAttributeMemory),
-            0xFEA0..=0xFEFF => Ok(AddrLocation::Unusable),
-            0xFF00..=0xFF7F => Ok(AddrLocation::IoRegisters),
-            0xFF80..=0xFFFE => Ok(AddrLocation::ZeroPage),
-            _ => Err(format!("0x{:X} address out of range ", address)),
+            0x0000..=0x3FFF => AddrLocation::RomBank0,
+            0x4000..=0x7FFF => AddrLocation::RomBank1,
+            0x8000..=0x97FF => AddrLocation::ChrRam,
+            0x9800..=0x9BFF => AddrLocation::BgMap1,
+            0x9C00..=0x9FFF => AddrLocation::BgMap2,
+            0xA000..=0xBFFF => AddrLocation::CartRam,
+            0xC000..=0xCFFF => AddrLocation::RamBank0,
+            0xD000..=0xDFFF => AddrLocation::RamBank1To7,
+            0xE000..=0xFDFF => AddrLocation::EchoRam,
+            0xFE00..=0xFE9F => AddrLocation::ObjectAttributeMemory,
+            0xFEA0..=0xFEFF => AddrLocation::Unusable,
+            0xFF00..=0xFF7F => AddrLocation::IoRegisters,
+            0xFF80..=0xFFFE => AddrLocation::ZeroPage,
+            0xFFFF => AddrLocation::IeRegister,
+            _ => unreachable!("Size of u16 doesn't allow bigger values"),
         }
     }
 }
 
 impl Bus {
     pub fn new(cart: Cart, ram: Ram) -> Self {
-        Self { cart, ram }
+        Self {
+            cart,
+            ram,
+            ie_register: 0,
+        }
     }
 
     pub fn read(&self, address: u16) -> u8 {
-        let location = AddrLocation::try_from(address).unwrap();
+        let location = AddrLocation::from(address);
 
         match location {
             AddrLocation::RomBank0 | AddrLocation::RomBank1 | AddrLocation::CartRam => {
-                return self.cart.read(address)
+                self.cart.read(address)
             }
-            AddrLocation::ChrRam => {}
-            AddrLocation::BgMap1 => {}
-            AddrLocation::BgMap2 => {}
-            AddrLocation::RamBank0 => {}
-            AddrLocation::RamBank1To7 | AddrLocation::EchoRam => {
-                return self.ram.w_ram_read(address)
-            }
-            AddrLocation::ObjectAttributeMemory => {}
-            AddrLocation::Unusable => {}
-            AddrLocation::IoRegisters => {}
-            AddrLocation::ZeroPage => {}
+            AddrLocation::ChrRam => panic!("Can't bus read read address {address}"),
+            AddrLocation::BgMap1 => panic!("Can't bus read read address {address}"),
+            AddrLocation::BgMap2 => panic!("Can't bus read read address {address}"),
+            AddrLocation::RamBank0 => panic!("Can't bus read read address {address}"),
+            AddrLocation::RamBank1To7 | AddrLocation::EchoRam => self.ram.w_ram_read(address),
+            AddrLocation::ObjectAttributeMemory => panic!("Can't bus read read address {address}"),
+            AddrLocation::Unusable => 0,
+            AddrLocation::IoRegisters => panic!("Can't bus read read address {address}"),
+            AddrLocation::ZeroPage => panic!("Can't bus read read address {address}"),
+            AddrLocation::IeRegister => self.ie_register,
         }
-
-        panic!("Can't bus read read address {address}");
     }
 
     pub fn write(&mut self, address: u16, value: u8) {
-        let location = AddrLocation::try_from(address).unwrap();
+        let location = AddrLocation::from(address);
 
         match location {
             AddrLocation::RomBank0 | AddrLocation::RomBank1 | AddrLocation::CartRam => {
-                self.cart.write(address, value);
-                return;
+                self.cart.write(address, value)
             }
-            AddrLocation::ChrRam => {}
-            AddrLocation::BgMap1 => {}
-            AddrLocation::BgMap2 => {}
+            AddrLocation::ChrRam => panic!("Can't bus write address {address}"),
+            AddrLocation::BgMap1 => panic!("Can't bus write address {address}"),
+            AddrLocation::BgMap2 => panic!("Can't bus write address {address}"),
             AddrLocation::RamBank0 | AddrLocation::RamBank1To7 => {
-                self.ram.w_ram_write(address, value);
-                return;
+                self.ram.w_ram_write(address, value)
             }
-            AddrLocation::EchoRam => {}
-            AddrLocation::ObjectAttributeMemory => {}
-            AddrLocation::Unusable => {}
-            AddrLocation::IoRegisters => {}
-            AddrLocation::ZeroPage => {}
+            AddrLocation::EchoRam => panic!("Can't bus write address {address}"),
+            AddrLocation::ObjectAttributeMemory => panic!("Can't bus write address {address}"),
+            AddrLocation::Unusable => (),
+            AddrLocation::IoRegisters => panic!("Can't bus write address {address}"),
+            AddrLocation::ZeroPage => self.ram.h_ram_write(address, value),
+            AddrLocation::IeRegister => self.ie_register = value,
         }
-
-        panic!("Can't bus write address {address}");
     }
 
     pub fn read16(&self, address: u16) -> u16 {
@@ -110,5 +110,17 @@ impl Bus {
     pub fn write16(&mut self, address: u16, value: u16) {
         self.write(address + 1, ((value >> 8) & 0xFF) as u8);
         self.write(address, (value & 0xFF) as u8);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ie_register() {
+        let location: AddrLocation = 0xFFFF.into();
+
+        assert_eq!(location, AddrLocation::IeRegister);
     }
 }
