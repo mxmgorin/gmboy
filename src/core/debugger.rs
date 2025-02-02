376516c6f69e54a72d98a8bb290189ea1a772b12
Maxim Gorin <onlinexmax@gmail.com>
2025-02-02 22:35:51 +0300

    Fix print of tikcs

--- src/core/debugger.rs
+++ src/core/debugger.rs
@@ -42,7 +42,7 @@ impl Debugger {
     ) {
         println!(
             "{:08X} - {:04X}: {:<20} ({:02X} {:02X} {:02X}) A: {:02X} F: {} BC: {:02X}{:02X} DE: {:02X}{:02X} HL: {:02X}{:02X}",
-            0,
+            cpu.ticks,
             pc,
             instruction.to_asm_string(cpu, fetched_data),
             opcode,
