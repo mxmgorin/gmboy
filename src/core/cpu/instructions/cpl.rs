884295c0d565f2d20617c6739d919bf40bd03c6f
Maxim Gorin <onlinexmax@gmail.com>
2025-02-03 11:56:51 +0300

    Impl cpl

--- src/core/cpu/instructions/cpl.rs
+++ src/core/cpu/instructions/cpl.rs
@@ -2,12 +2,19 @@ use crate::core::cpu::instructions::common::{AddressMode, ExecutableInstruction}
 use crate::core::cpu::Cpu;
 use crate::cpu::instructions::common::FetchedData;
 
+/// ComPLement accumulator (A = ~A); also called bitwise NOT.
+/// Cycles: 1
+/// Bytes: 1
+/// Flags:
+/// N 1
+/// H 1
 #[derive(Debug, Clone, Copy)]
 pub struct CplInstruction;
 
 impl ExecutableInstruction for CplInstruction {
-    fn execute(&self, _cpu: &mut Cpu, _fetched_data: FetchedData) {
-        unimplemented!("CplInstruction")
+    fn execute(&self, cpu: &mut Cpu, _fetched_data: FetchedData) {
+        cpu.registers.a = !cpu.registers.a;
+        cpu.registers.set_flags(None, 1.into(), 1.into(), None);
     }
 
     fn get_address_mode(&self) -> AddressMode {
