4f1af7d221fc8cac5cbe46b5330bd5349f2d2a6f
Maxim Gorin <onlinexmax@gmail.com>
2025-02-04 11:19:33 +0300

    Fix wram size

--- src/core/bus/ram.rs
+++ src/core/bus/ram.rs
@@ -1,4 +1,4 @@
-const W_RAM_SIZE: usize = 0x8000;
+const W_RAM_SIZE: usize = 0x2000;
 const H_RAM_SIZE: usize = 0x80;
 const W_RAM_ADDR_OFFSET: usize = 0xC000;
 const H_RAM_ADDR_OFFSET: usize = 0xFF80;
