44a6f5379b6c336474fe982ea541889e5636ef55
Maxim Gorin <onlinexmax@gmail.com>
2025-02-11 15:32:10 +0300

    Add tests for timer

--- src/core/auxiliary/timer.rs
+++ src/core/auxiliary/timer.rs
@@ -1,3 +1,4 @@
+use crate::auxiliary::clock::T_CYCLES_PER_M_CYCLE;
 use crate::cpu::interrupts::{InterruptType, Interrupts};
 
 const DIV_ADDRESS: u16 = 0xFF04;
@@ -11,6 +12,8 @@ pub struct Timer {
     tima: u8,
     tma: u8,
     tac: u8,
+    tima_overflow: bool,
+    interrupt_delay: usize,
 }
 
 impl Default for Timer {
@@ -26,6 +29,8 @@ impl Timer {
             tima: 0,
             tma: 0,
             tac: 0,
+            tima_overflow: false,
+            interrupt_delay: 0,
         }
     }
 
@@ -34,37 +39,44 @@ impl Timer {
         let prev_div = self.div;
         self.div = self.div.wrapping_add(1);
 
-        let mut timer_update = false;
-
-        // 0b00 (4096 Hz): div bit 9
-        // 0b01 (262144 Hz): div bit 3
-        // 0b10 (65536 Hz): div bit 5
-        // 0b11 (16384 Hz): div bit 7
-        // detect when bit N transitions from 1 to 0 between the previous DIV and current DIV values
-        match self.tac & 0b11 {
-            0b00 => {
-                timer_update = (prev_div & (1 << 9)) != 0 && (self.div & (1 << 9)) == 0;
-            }
-            0b01 => {
-                timer_update = (prev_div & (1 << 3)) != 0 && (self.div & (1 << 3)) == 0;
-            }
-            0b10 => {
-                timer_update = (prev_div & (1 << 5)) != 0 && (self.div & (1 << 5)) == 0;
-            }
-            0b11 => {
-                timer_update = (prev_div & (1 << 7)) != 0 && (self.div & (1 << 7)) == 0;
-            }
-            _ => {}
-        }
-
-        // Update TIMA if the timer is enabled and a timer update is triggered
-        if timer_update && (self.tac & (1 << 2)) != 0 {
-            self.tima = self.tima.wrapping_add(1);
-
-            let is_overflow = self.tima == 0xFF;
-            if is_overflow {
+        // TIMA overflowed during the last cycle
+        if self.tima_overflow {
+            if self.interrupt_delay == 0 {
+                self.tima_overflow = false;
                 self.tima = self.tma;
                 interrupts.request_interrupt(InterruptType::Timer);
+            } else {
+                self.interrupt_delay -= 1;
+            }
+        }
+
+        // detect when bit N transitions from 1 to 0 between the previous DIV and current DIV values
+        let timer_update = match self.tac & 0b11 {
+            // 0b00 (4096 Hz): div bit 9, increment every 256 M-cycles
+            0b00 => (prev_div & (1 << 9)) != 0 && (self.div & (1 << 9)) == 0,
+            // 0b01 (262144 Hz): div bit 3, increment every 4 M-cycles
+            0b01 => (prev_div & (1 << 3)) != 0 && (self.div & (1 << 3)) == 0,
+            // 0b10 (65536 Hz): div bit 5, increment every 16 M-cycles
+            0b10 => (prev_div & (1 << 5)) != 0 && (self.div & (1 << 5)) == 0,
+            // 0b11 (16384 Hz): div bit 7, increment every 64 M-cycles
+            0b11 => (prev_div & (1 << 7)) != 0 && (self.div & (1 << 7)) == 0,
+            _ => false,
+        };
+
+        // If bit 2 of TAC is set to 0 then the timer is disabled
+        let is_enabled = self.tac & (1 << 2) != 0;
+
+        // Update TIMA if the timer is enabled and a timer update is triggered
+        if timer_update && is_enabled {
+            self.tima = self.tima.wrapping_add(1);
+            self.tima_overflow = self.tima == 0xFF;
+
+            if self.tima_overflow {
+                // Timer interrupt is delayed 1 cycle (4 clocks) from the TIMA
+                // overflow. The TMA reload to TIMA is also delayed. For one cycle,
+                // after overflowing TIMA, the value in TIMA is 00h, not TMA.
+                self.tima = 0x00;
+                self.interrupt_delay = T_CYCLES_PER_M_CYCLE;
             }
         }
     }
@@ -125,5 +137,64 @@ impl TimerAddress {
 
     pub const fn get_end() -> u16 {
         TAC_ADDRESS
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::auxiliary::clock::T_CYCLES_PER_M_CYCLE;
+    use crate::auxiliary::timer::Timer;
+    use crate::cpu::interrupts::Interrupts;
+
+    #[test]
+    pub fn test_timer_tima_inc_256() {
+        let mut timer = Timer::new();
+        timer.tac = 0b100;
+        let mut interrupts = Interrupts::default();
+
+        for _ in 0..(256 * T_CYCLES_PER_M_CYCLE) {
+            timer.tick(&mut interrupts)
+        }
+
+        assert_eq!(timer.tima, 1);
+    }
+
+    #[test]
+    pub fn test_timer_tima_inc_4() {
+        let mut timer = Timer::new();
+        timer.tac = 0b101;
+        let mut interrupts = Interrupts::default();
+
+        for _ in 0..T_CYCLES_PER_M_CYCLE {
+            timer.tick(&mut interrupts)
+        }
+
+        assert_eq!(timer.tima, 1);
+    }
+
+    #[test]
+    pub fn test_timer_tima_inc_16() {
+        let mut timer = Timer::new();
+        timer.tac = 0b110;
+        let mut interrupts = Interrupts::default();
+
+        for _ in 0..(16 * T_CYCLES_PER_M_CYCLE) {
+            timer.tick(&mut interrupts)
+        }
+
+        assert_eq!(timer.tima, 1);
+    }
+
+    #[test]
+    pub fn test_timer_tima_inc_64() {
+        let mut timer = Timer::new();
+        timer.tac = 0b111;
+        let mut interrupts = Interrupts::default();
+
+        for _ in 0..(64 * T_CYCLES_PER_M_CYCLE) {
+            timer.tick(&mut interrupts)
+        }
+
+        assert_eq!(timer.tima, 1);
     }
 }
