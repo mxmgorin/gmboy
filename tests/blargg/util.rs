b30b066cca5e3905ace71949bfb7e164f6a22af6
Maxim Gorin <onlinexmax@gmail.com>
2025-02-19 11:24:12 +0300

    Add oam bug blargg tests

--- tests/blargg/util.rs
+++ tests/blargg/util.rs
@@ -6,7 +6,7 @@ use rusty_gb_emu::emu::{read_bytes, EmuCtx};
 use std::path::PathBuf;
 use std::time::{Duration, Instant};
 
-pub fn run_blargg_rom(
+pub fn run_blargg_rom_serial(
     name: &str,
     category: Option<TestRomCategory>,
     timeout: Duration,
@@ -33,6 +33,38 @@ pub fn run_blargg_rom(
     }
 }
 
+pub fn run_blargg_rom_memory(
+    name: &str,
+    category: Option<TestRomCategory>,
+    timeout: Duration,
+) -> Result<(), String> {
+    let path = get_blargg_rom_path(&format!("{}.gb", name), category);
+    let cart = Cart::new(read_bytes(path.to_str().unwrap())?)?;
+    let mut cpu = Cpu::new(Bus::new(cart));
+    let instant = Instant::now();
+
+    loop {
+        cpu.step(&mut EmuCtx::default(), None)?;
+        let b1 = cpu.bus.read(0xA001);
+        let b2 = cpu.bus.read(0xA002);
+        let b3 = cpu.bus.read(0xA003);
+        let result = cpu.bus.read(0xA000);
+
+        if b1 == 0xDE && b2 == 0xB0 && b3 == 0x61 && result != 0x80 {
+            match result {
+                0 => return Ok(()),
+                1 => return Err(format!("{result}: failed")),
+                2 => return Err(format!("{result}: error2")),
+                _ => unreachable!(),
+            }
+        }
+
+        if instant.elapsed() > timeout {
+            return Err(format!("TIMEOUT: {}", timeout.as_secs()));
+        }
+    }
+}
+
 pub fn assert_result(name: &str, category: Option<TestRomCategory>, result: Result<(), String>) {
     let name = if let Some(category) = category {
         format!("{:?} {}", category, name)
@@ -51,6 +83,7 @@ pub fn assert_result(name: &str, category: Option<TestRomCategory>, result: Resu
 pub enum TestRomCategory {
     CpuInstructions,
     MemTiming,
+    OamBug,
 }
 
 pub fn get_blargg_rom_path(rom_name: &str, category: Option<TestRomCategory>) -> PathBuf {
@@ -60,6 +93,7 @@ pub fn get_blargg_rom_path(rom_name: &str, category: Option<TestRomCategory>) ->
         let dir = match category {
             TestRomCategory::CpuInstructions => "cpu_instrs",
             TestRomCategory::MemTiming => "mem_timing",
+            TestRomCategory::OamBug => "oam_bug",
         };
 
         root = root.join(dir);
