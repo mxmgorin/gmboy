65cac07013cbe8c4e1630d537be8fa93e1cf29ee
Maxim Gorin <onlinexmax@gmail.com>
2025-02-28 22:12:08 +0300

    Refactoring

--- src/ppu/fetcher.rs
+++ src/ppu/fetcher.rs
@@ -1,5 +1,4 @@
 use crate::bus::Bus;
-use crate::ppu::lcd::Lcd;
 use crate::ppu::sprite::SpriteFetcher;
 use crate::ppu::tile::{get_color_index, Pixel, TILE_BITS_COUNT, TILE_HEIGHT, TILE_WIDTH};
 use crate::ppu::{LCD_X_RES, LCD_Y_RES};
@@ -13,21 +12,16 @@ pub struct BgwFetchedData {
     pub tile_idx: u8,
     pub byte1: u8,
     pub byte2: u8,
-    pub is_window: bool,
     pub map_x: u8,
     pub map_y: u8,
+    pub data_area: u16,
 }
 
 impl BgwFetchedData {
-    pub fn get_bgw_data_addr(&mut self, lcd: &Lcd) -> u16 {
-        if self.is_window {
-            self.map_y = lcd.ly.wrapping_sub(lcd.window.y);
-        }
-
+    pub fn get_data_addr(&mut self) -> u16 {
         let tile_y = (self.map_y % TILE_HEIGHT as u8) * 2;
 
-        lcd.control
-            .bgw_data_area()
+        self.data_area
             .wrapping_add(self.tile_idx as u16 * 16)
             .wrapping_add(tile_y as u16)
     }
@@ -91,21 +85,27 @@ impl PixelFetcher {
         match self.fetch_step {
             FetchStep::Tile => {
                 if bus.io.lcd.control.bgw_enabled() {
-                    self.bgw_fetched_data.map_y = bus.io.lcd.ly.wrapping_add(bus.io.lcd.scroll_y);
-                    self.bgw_fetched_data.map_x = self.fetch_x.wrapping_add(bus.io.lcd.scroll_x);
-                    let addr = bus.io.lcd.control.bg_map_area()
-                        + (self.bgw_fetched_data.map_x as u16 / TILE_WIDTH)
-                        + ((self.bgw_fetched_data.map_y as u16 / TILE_HEIGHT) * 32);
-                    self.bgw_fetched_data.tile_idx = bus.read(addr);
-                    self.bgw_fetched_data.is_window = false;
+                    self.bgw_fetched_data.data_area = bus.io.lcd.control.bgw_data_area();
 
                     if let Some(tile_idx) = bus.io.lcd.window.get_tile_idx(self.fetch_x as u16, bus)
                     {
+                        self.bgw_fetched_data.map_y =
+                            bus.io.lcd.ly.wrapping_add(bus.io.lcd.window.y);
+                        self.bgw_fetched_data.map_x =
+                            self.fetch_x.wrapping_add(bus.io.lcd.window.x);
                         self.bgw_fetched_data.tile_idx = tile_idx;
-                        self.bgw_fetched_data.is_window = true;
+                    } else {
+                        self.bgw_fetched_data.map_y =
+                            bus.io.lcd.ly.wrapping_add(bus.io.lcd.scroll_y);
+                        self.bgw_fetched_data.map_x =
+                            self.fetch_x.wrapping_add(bus.io.lcd.scroll_x);
+                        let addr = bus.io.lcd.control.bg_map_area()
+                            + (self.bgw_fetched_data.map_x as u16 / TILE_WIDTH)
+                            + ((self.bgw_fetched_data.map_y as u16 / TILE_HEIGHT) * 32);
+                        self.bgw_fetched_data.tile_idx = bus.read(addr);
                     }
 
-                    if bus.io.lcd.control.bgw_data_area() == 0x8800 {
+                    if self.bgw_fetched_data.data_area == 0x8800 {
                         self.bgw_fetched_data.tile_idx =
                             self.bgw_fetched_data.tile_idx.wrapping_add(128);
                     }
@@ -120,12 +120,13 @@ impl PixelFetcher {
                 self.fetch_x = self.fetch_x.wrapping_add(TILE_WIDTH as u8);
             }
             FetchStep::Data0 => {
-                self.bgw_fetched_data.byte1 = bus.read(self.bgw_fetched_data.get_bgw_data_addr(&bus.io.lcd));
+                self.bgw_fetched_data.byte1 = bus.read(self.bgw_fetched_data.get_data_addr());
                 self.sprite_fetcher.fetch_sprite_data(bus, 0);
                 self.fetch_step = FetchStep::Data1;
             }
             FetchStep::Data1 => {
-                self.bgw_fetched_data.byte2 = bus.read(self.bgw_fetched_data.get_bgw_data_addr(&bus.io.lcd) + 1);
+                self.bgw_fetched_data.byte2 =
+                    bus.read(self.bgw_fetched_data.get_data_addr() + 1);
                 self.sprite_fetcher.fetch_sprite_data(bus, 1);
                 self.fetch_step = FetchStep::Idle;
             }
