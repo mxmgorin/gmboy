4834cfe7366158ac63d6f77cc696ae0ae10c8b91
Maxim Gorin <onlinexmax@gmail.com>
2025-03-17 16:37:59 +0300

    Refactoring

--- src/auxiliary/joypad.rs
+++ src/auxiliary/joypad.rs
@@ -1,9 +1,9 @@
 pub const JOYPAD_ADDR: u16 = 0xFF00;
 
-pub const A_RIGHT_BIT: u8 = 0x00;
-pub const B_LEFT_BIT: u8 = 0x01;
-pub const SELECT_UP_BIT: u8 = 0x02;
-pub const START_DOWN_BIT: u8 = 0x03;
+pub const A_OR_RIGHT_BIT: u8 = 0x00;
+pub const B_OR_LEFT_BIT: u8 = 0x01;
+pub const SELECT_OR_UP_BIT: u8 = 0x02;
+pub const START_OR_DOWN_BIT: u8 = 0x03;
 
 pub const SELECT_DIRECTIONS_BIT: u8 = 0x04;
 pub const SELECT_ACTIONS_BIT: u8 = 0x05;
@@ -26,24 +26,24 @@ pub struct Joypad {
 impl Joypad {
     pub fn get_byte(&self) -> u8 {
         if self.actions_selected {
-            return ((!self.a as u8) << A_RIGHT_BIT)
-                | ((!self.b as u8) << B_LEFT_BIT)
-                | ((!self.select as u8) << SELECT_UP_BIT)
-                | ((!self.start as u8) << START_DOWN_BIT);
+            return ((!self.a as u8) << A_OR_RIGHT_BIT)
+                | ((!self.b as u8) << B_OR_LEFT_BIT)
+                | ((!self.select as u8) << SELECT_OR_UP_BIT)
+                | ((!self.start as u8) << START_OR_DOWN_BIT);
         }
 
         if self.directions_selected {
-            return ((!self.right as u8) << A_RIGHT_BIT)
-                | ((!self.left as u8) << B_LEFT_BIT)
-                | ((!self.up as u8) << SELECT_UP_BIT)
-                | ((!self.down as u8) << START_DOWN_BIT);
+            return ((!self.right as u8) << A_OR_RIGHT_BIT)
+                | ((!self.left as u8) << B_OR_LEFT_BIT)
+                | ((!self.up as u8) << SELECT_OR_UP_BIT)
+                | ((!self.down as u8) << START_OR_DOWN_BIT);
         }
 
         0xFF
     }
 
     pub fn set_byte(&mut self, value: u8) {
-        self.directions_selected = (value >> SELECT_DIRECTIONS_BIT) & 0x01 == 0;
-        self.actions_selected = (value >> SELECT_ACTIONS_BIT) & 0x01 == 0;
+        self.directions_selected = (value & 0x10) == 0;
+        self.actions_selected = (value & 0x20) == 0;
     }
 }
