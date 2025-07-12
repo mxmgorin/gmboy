b4199488e42ed19e40d4420379fbb7460ed92962
Troidem <onlinexmax@gmail.com>
2025-07-12 18:31:56 +0300

    Move cpu into emu

--- src/emu.rs
+++ src/emu.rs
@@ -26,15 +26,15 @@ pub struct EmuSaveState {
 }
 
 pub struct Emu {
-    pub clock: Clock,
-    pub ppu: Ppu,
-    pub debugger: Option<Debugger>,
-    pub ui: Ui,
-
     pub ctx: EmuCtx,
+    pub cpu: Cpu,
+    pub ui: Ui,
 }
 
 pub struct EmuCtx {
+    pub ppu: Ppu,
+    pub clock: Clock,
+    pub debugger: Option<Debugger>,
     pub speed_multiplier: f64,
     pub state: EmuState,
     pub config: Config,
@@ -63,6 +63,9 @@ pub enum RunMode {
 impl EmuCtx {
     pub fn new(config: Config) -> EmuCtx {
         Self {
+            ppu: Ppu::default(),
+            clock: Clock::default(),
+            debugger: Some(Debugger::new(CpuLogType::None, false)),
             speed_multiplier: 1.0,
             state: EmuState::WaitCart,
             config,
@@ -78,7 +81,7 @@ impl EmuCtx {
     }
 }
 
-impl CpuCallback for Emu {
+impl CpuCallback for EmuCtx {
     fn m_cycles(&mut self, m_cycles: usize, bus: &mut Bus) {
         self.clock.m_cycles(m_cycles, bus, &mut self.ppu);
     }
@@ -134,9 +137,7 @@ impl UiEventHandler for EmuCtx {
 impl Emu {
     pub fn new(config: Config) -> Result<Self, String> {
         Ok(Self {
-            ppu: Ppu::default(),
-            clock: Clock::default(),
-            debugger: Some(Debugger::new(CpuLogType::None, false)),
+            cpu: Cpu::new(Bus::with_bytes(vec![])),
             ui: Ui::new(config.graphics.clone(), false)?,
             ctx: EmuCtx::new(config),
         })
@@ -154,24 +155,24 @@ impl Emu {
         };
 
         if self.ctx.speed_multiplier != speed_multiplier {
-            self.clock.reset();
+            self.ctx.clock.reset();
         }
 
         self.ctx.speed_multiplier = speed_multiplier;
 
         let emulated_time_ns =
-            (self.clock.t_cycles as f64 * CYCLE_TIME / speed_multiplier).round() as u64;
+            (self.ctx.clock.t_cycles as f64 * CYCLE_TIME / speed_multiplier).round() as u64;
 
         Duration::from_nanos(emulated_time_ns)
     }
 
-    fn tick(&mut self, cpu: &mut Cpu) -> Result<(), String> {
-        let prev_m_cycles = self.clock.get_m_cycles();
+    fn tick(&mut self) -> Result<(), String> {
+        let prev_m_cycles = self.ctx.clock.get_m_cycles();
 
-        while self.clock.get_m_cycles() - prev_m_cycles < CYCLES_PER_FRAME {
-            cpu.step(self)?;
+        while self.ctx.clock.get_m_cycles() - prev_m_cycles < CYCLES_PER_FRAME {
+            self.cpu.step(&mut self.ctx)?;
 
-            if let Some(debugger) = self.debugger.as_mut() {
+            if let Some(debugger) = self.ctx.debugger.as_mut() {
                 if !debugger.get_serial_msg().is_empty() {
                     println!("Serial: {}", debugger.get_serial_msg());
                 }
@@ -180,22 +181,22 @@ impl Emu {
             if !self.ctx.config.emulation.is_muted
                 && EmuState::Running(RunMode::Normal) == self.ctx.state
             {
-                self.ui.audio.update(&mut cpu.bus.io.apu)?;
+                self.ui.audio.update(&mut self.cpu.bus.io.apu)?;
             }
         }
 
-        let real_elapsed = self.clock.start_time.elapsed();
+        let real_elapsed = self.ctx.clock.start_time.elapsed();
         let emulated_time = self.calc_emulated_time();
 
         if emulated_time > real_elapsed {
             spin_wait(emulated_time - real_elapsed);
         }
 
-        if self.ctx.prev_frame != self.ppu.current_frame {
-            self.ui.draw(&mut self.ppu, &cpu.bus);
+        if self.ctx.prev_frame != self.ctx.ppu.current_frame {
+            self.ui.draw(&mut self.ctx.ppu, &self.cpu.bus);
         }
 
-        self.ctx.prev_frame = self.ppu.current_frame;
+        self.ctx.prev_frame = self.ctx.ppu.current_frame;
 
         Ok(())
     }
@@ -211,8 +212,6 @@ impl Emu {
             self.ctx.state = EmuState::LoadCart(cart_path);
         }
 
-        let mut cpu = Cpu::new(Bus::with_bytes(vec![]));
-
         loop {
             if self.ctx.state == EmuState::Paused || self.ctx.state == EmuState::WaitCart {
                 let text = if self.ctx.state == EmuState::Paused {
@@ -221,7 +220,7 @@ impl Emu {
                     "DROP FILE"
                 };
                 self.ui.draw_text(text);
-                self.ui.handle_events(&mut cpu.bus, &mut self.ctx);
+                self.ui.handle_events(&mut self.cpu.bus, &mut self.ctx);
                 thread::sleep(Duration::from_millis(100));
                 continue;
             }
@@ -236,7 +235,7 @@ impl Emu {
 
                 let mut bus = Bus::new(cart);
                 bus.io.lcd.set_pallet(self.ui.curr_palette);
-                cpu = Cpu::new(bus);
+                self.cpu = Cpu::new(bus);
 
                 self.ctx.config.last_cart_path = Some(path.to_owned());
                 self.ctx.state = EmuState::Running(RunMode::Normal);
@@ -245,19 +244,19 @@ impl Emu {
 
             if let EmuState::Running(RunMode::Rewind) = &self.ctx.state {
                 if let Some(state) = self.ctx.rewind_buffer.pop_back() {
-                    self.load_state(&mut cpu, state);
+                    load_state(self, state);
                 }
             }
 
-            self.ui.handle_events(&mut cpu.bus, &mut self.ctx);
-            self.tick(&mut cpu)?;
+            self.ui.handle_events(&mut self.cpu.bus, &mut self.ctx);
+            self.tick()?;
 
-            if self.ctx.config.emulation.rewind_size > 0 && self.clock.t_cycles % 5000 == 0 {
+            if self.ctx.config.emulation.rewind_size > 0 && self.ctx.clock.t_cycles % 5000 == 0 {
                 if self.ctx.rewind_buffer.len() > self.ctx.config.emulation.rewind_size {
                     self.ctx.rewind_buffer.pop_front();
                 }
 
-                self.ctx.rewind_buffer.push_back(self.save_state(&cpu));
+                self.ctx.rewind_buffer.push_back(self.save_state(&self.cpu));
             }
         }
 
@@ -266,24 +265,24 @@ impl Emu {
 
     pub fn save_state(&self, cpu: &Cpu) -> EmuSaveState {
         EmuSaveState {
-            clock: self.clock.clone(),
+            clock: self.ctx.clock.clone(),
             cpu_without_bus: cpu.clone_without_bus(),
             bus_without_cart: cpu.bus.clone_without_cart(),
             cart_mbc: cpu.bus.cart.mbc.clone(),
         }
     }
+}
 
-    pub fn load_state(&mut self, cpu: &mut Cpu, save_state: EmuSaveState) {
-        let mut state_cpu = save_state.cpu_without_bus; // reconstruct cpu
-        state_cpu.bus = save_state.bus_without_cart;
-        state_cpu.bus.io.joypad = Joypad::default(); // reset controls
-        state_cpu.bus.cart.mbc = save_state.cart_mbc; // reconstruct cart
-        state_cpu.bus.cart.data = cpu.bus.cart.data.clone();
+fn load_state(emu: &mut Emu, save_state: EmuSaveState) {
+    let mut state_cpu = save_state.cpu_without_bus; // reconstruct cpu
+    state_cpu.bus = save_state.bus_without_cart;
+    state_cpu.bus.io.joypad = Joypad::default(); // reset controls
+    state_cpu.bus.cart.mbc = save_state.cart_mbc; // reconstruct cart
+    state_cpu.bus.cart.data = emu.cpu.bus.cart.data.clone();
 
-        *cpu = state_cpu;
-        self.clock = save_state.clock;
-        self.ctx.reset();
-    }
+    emu.cpu = state_cpu;
+    emu.ctx.clock = save_state.clock;
+    emu.ctx.reset();
 }
 
 pub fn read_cart(file: &str) -> Result<Cart, String> {
