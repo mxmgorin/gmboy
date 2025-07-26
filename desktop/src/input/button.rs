22016c1c5f093cd57c5fe6ea2e60213cf7ac5464
Troidem <onlinexmax@gmail.com>
2025-07-26 18:24:42 +0300

    Fix opening menu by combo

--- desktop/src/input/button.rs
+++ desktop/src/input/button.rs
@@ -1,22 +1,14 @@
-use crate::Emu;
 use crate::app::{App, AppCmd, AppState};
+use crate::Emu;
 
-pub fn handle_start(is_pressed: bool, app: &mut App, emu: &mut Emu) -> Option<AppCmd> {
-    if app.state == AppState::Paused && !is_pressed {
-        return app.menu.select(&app.config);
-    } else {
-        emu.runtime.bus.io.joypad.start = is_pressed;
-    }
+pub fn handle_start(is_pressed: bool, _app: &mut App, emu: &mut Emu) -> Option<AppCmd> {
+    emu.runtime.bus.io.joypad.start = is_pressed;
 
     None
 }
 
-pub fn handle_select(is_pressed: bool, app: &mut App, emu: &mut Emu) -> Option<AppCmd> {
-    if app.state == AppState::Paused && !is_pressed {
-        app.menu.cancel();
-    } else {
-        emu.runtime.bus.io.joypad.select = is_pressed
-    }
+pub fn handle_select(is_pressed: bool, _app: &mut App, emu: &mut Emu) -> Option<AppCmd> {
+    emu.runtime.bus.io.joypad.select = is_pressed;
 
     None
 }
