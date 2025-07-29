4bfc429a3b1bff67ed9d5a9dceb9c15b6c2620cd
Troidem <onlinexmax@gmail.com>
2025-07-29 13:48:42 +0300

    Add config for triggers

--- desktop/src/input/gamepad.rs
+++ desktop/src/input/gamepad.rs
@@ -1,9 +1,8 @@
 use crate::app::{App, AppCmd};
+use crate::input::button::handle_emu_btn;
 use crate::input::combos::ComboTracker;
 use crate::Emu;
-use core::emu::state::SaveStateCmd;
 use sdl2::controller::Button;
-use crate::input::button::handle_emu_btn;
 
 pub fn handle_gamepad(
     state: &mut ComboTracker,
@@ -27,22 +26,15 @@ pub fn handle_gamepad(
     Some(cmd.to_owned())
 }
 
-pub fn handle_gamepad_axis(_app: &App, axis_idx: u8, value: i16) -> Option<AppCmd> {
-    const LEFT: u8 = 2;
-    const RIGHT: u8 = 5;
-    const THRESHOLD: i16 = 25_000;
-    let is_pressed = value > THRESHOLD;
-
-    if axis_idx == LEFT && !is_pressed {
-        return Some(AppCmd::SaveState(
-            SaveStateCmd::Load,
-            None
-        ));
-    } else if axis_idx == RIGHT && !is_pressed {
-        return Some(AppCmd::SaveState(
-            SaveStateCmd::Create,
-            None
-        ));
+pub fn handle_gamepad_axis(app: &App, axis_idx: u8, value: i16) -> Option<AppCmd> {
+    if axis_idx == app.config.input.bindings.left_trigger.code
+        && !app.config.input.bindings.left_trigger.is_pressed(value)
+    {
+        return app.config.input.bindings.left_trigger.cmd.clone();
+    } else if axis_idx == app.config.input.bindings.right_trigger.code
+        && !app.config.input.bindings.right_trigger.is_pressed(value)
+    {
+        return app.config.input.bindings.right_trigger.cmd.clone();
     }
 
     None
