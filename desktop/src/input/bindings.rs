97ce47fc6ebb4b67574e862667ad217595c486f0
Troidem <onlinexmax@gmail.com>
2025-07-29 08:14:51 +0300

    Add gamepad bindings

--- desktop/src/input/bindings.rs
+++ desktop/src/input/bindings.rs
@@ -1,20 +1,106 @@
+use std::fmt;
 use crate::app::AppCmd;
 use sdl2::controller::Button;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::option::Option;
+use serde::de::{Error, MapAccess, Visitor};
+use serde::ser::SerializeMap;
+use crate::input::{all_buttons, button_to_str, str_to_button};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Bindings {
+    pub gamepad: GamepadBindings,
     pub gamepad_combos: Vec<ButtonCombo>,
 }
 
 impl Default for Bindings {
     fn default() -> Self {
         Self {
+            gamepad: GamepadBindings::new(),
             gamepad_combos: vec![
                 ButtonCombo::new(Button::Start, Button::Back, AppCmd::ToggleMenu),
                 ButtonCombo::new(Button::Start, Button::Guide, AppCmd::ToggleMenu),
             ],
         }
+    }
+}
+#[derive(Debug, Clone)]
+pub struct GamepadBindings {
+    map: [Option<AppCmd>; GamepadBindings::BUTTON_COUNT],
+}
+
+impl GamepadBindings {
+    pub const BUTTON_COUNT: usize = 15;
+
+    #[inline(always)]
+    fn idx(btn: Button) -> usize {
+        btn as usize
+    }
+
+    pub fn new() -> Self {
+        Self { map: std::array::from_fn(|_| None) }
+    }
+
+    #[inline(always)]
+    pub fn get(&self, btn: Button) -> Option<&AppCmd> {
+        self.map.get(Self::idx(btn)).and_then(|x| x.as_ref())
+    }
+
+    #[inline(always)]
+    pub fn set(&mut self, btn: Button, action: AppCmd) {
+        self.map[Self::idx(btn)] = Some(action);
+    }
+}
+
+impl Serialize for GamepadBindings {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map_ser = serializer.serialize_map(None)?;
+        for btn in all_buttons() {
+            if let Some(cmd) = self.get(*btn) {
+                map_ser.serialize_entry(&button_to_str(*btn), &cmd)?;
+            }
+        }
+
+        map_ser.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for GamepadBindings {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct BindingsVisitor;
+
+        impl<'de> Visitor<'de> for BindingsVisitor {
+            type Value = GamepadBindings;
+
+            fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "a map of gamepad button names to AppCmd")
+            }
+
+            fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+            where
+                M: MapAccess<'de>,
+            {
+                let mut bindings = GamepadBindings::new();
+
+                while let Some((btn_name, cmd)) = access.next_entry::<String, AppCmd>()? {
+                    if let Some(btn) = str_to_button(&btn_name) {
+                        bindings.set(btn, cmd);
+                    } else {
+                        return Err(M::Error::custom(format!("Unknown button: {}", btn_name)));
+                    }
+                }
+
+                Ok(bindings)
+            }
+        }
+
+        deserializer.deserialize_map(BindingsVisitor)
     }
 }
 
--- desktop/src/input/mod.rs
+++ desktop/src/input/mod.rs
@@ -1,7 +1,76 @@
+use sdl2::controller::Button;
+
+pub mod bindings;
 pub mod button;
 pub mod combos;
+pub mod config;
 pub mod gamepad;
 pub mod handler;
 pub mod keyboard;
-pub mod config;
-mod bindings;
+
+pub fn button_to_str(btn: Button) -> &'static str {
+    match btn {
+        Button::A => "A",
+        Button::B => "B",
+        Button::X => "X",
+        Button::Y => "Y",
+        Button::Back => "Back",
+        Button::Guide => "Guide",
+        Button::Start => "Start",
+        Button::LeftStick => "LeftStick",
+        Button::RightStick => "RightStick",
+        Button::LeftShoulder => "LB",
+        Button::RightShoulder => "RB",
+        Button::DPadUp => "DPadUp",
+        Button::DPadDown => "DPadDown",
+        Button::DPadLeft => "DPadLeft",
+        Button::DPadRight => "DPadRight",
+        Button::Misc1 |
+        Button::Paddle1 |
+        Button::Paddle2 |
+        Button::Paddle3 |
+        Button::Paddle4 |
+        Button::Touchpad => { "" }
+    }
+}
+
+pub fn str_to_button(s: &str) -> Option<Button> {
+    match s {
+        "A" => Some(Button::A),
+        "B" => Some(Button::B),
+        "X" => Some(Button::X),
+        "Y" => Some(Button::Y),
+        "Back" => Some(Button::Back),
+        "Guide" => Some(Button::Guide),
+        "Start" => Some(Button::Start),
+        "LeftStick" => Some(Button::LeftStick),
+        "RightStick" => Some(Button::RightStick),
+        "LB" => Some(Button::LeftShoulder),
+        "RB" => Some(Button::RightShoulder),
+        "DPadUp" => Some(Button::DPadUp),
+        "DPadDown" => Some(Button::DPadDown),
+        "DPadLeft" => Some(Button::DPadLeft),
+        "DPadRight" => Some(Button::DPadRight),
+        _ => None,
+    }
+}
+
+pub fn all_buttons() -> &'static [Button] {
+    &[
+        Button::A,
+        Button::B,
+        Button::X,
+        Button::Y,
+        Button::Back,
+        Button::Guide,
+        Button::Start,
+        Button::LeftStick,
+        Button::RightStick,
+        Button::LeftShoulder,
+        Button::RightShoulder,
+        Button::DPadUp,
+        Button::DPadDown,
+        Button::DPadLeft,
+        Button::DPadRight,
+    ]
+}
