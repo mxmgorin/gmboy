560bc73c1522d533fe2473bd7141e4c58491d5f1
Troidem <onlinexmax@gmail.com>
2025-08-06 17:51:37 +0300

    feat(android): make full screen

--- android/app/src/main/java/com/mxmgorin/gmboy/MainActivity.java
+++ android/app/src/main/java/com/mxmgorin/gmboy/MainActivity.java
@@ -1,7 +1,32 @@
 package com.mxmgorin.gmboy;
 
+import android.os.Bundle;
+import android.view.View;
 import org.libsdl.app.SDLActivity;
 
 public class MainActivity extends SDLActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableImmersiveMode();
+    }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableImmersiveMode();
+        }
+    }
+
+    private void enableImmersiveMode() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
 }
