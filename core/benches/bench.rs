ccd2598867f28e7c9c325fea0917faa585e01ffe
Troidem <onlinexmax@gmail.com>
2025-08-11 20:36:50 +0300

    perf(oam)

--- core/benches/bench.rs
+++ core/benches/bench.rs
@@ -1,8 +1,11 @@
+use core::ppu::oam::OAM_ADDR_START;
 use core::apu::Apu;
 use core::auxiliary::timer::Timer;
 use core::bus::Bus;
 use core::cart::Cart;
 use core::cpu::interrupts::Interrupts;
+use core::ppu::oam::OamRam;
+use core::ppu::oam::OAM_ENTRIES_COUNT;
 use core::ppu::Ppu;
 use core::read_bytes;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
@@ -65,6 +68,34 @@ fn criterion_benchmark(c: &mut Criterion) {
             |mut apu| {
                 for i in 0..5_000_000 {
                     apu.push_buffer(i as f32, i as f32);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    let oam = OamRam::default();
+
+    c.bench_function("oam_read_5_000_000", |b| {
+        b.iter_batched(
+            || oam.clone(),
+            |oam| {
+                for i in 0..5_000_000 {
+                    let _ = oam.read((i % (OAM_ENTRIES_COUNT * 4)) as u16 + OAM_ADDR_START);
+                }
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    let oam = OamRam::default();
+
+    c.bench_function("oam_write_5_000_000", |b| {
+        b.iter_batched(
+            || oam.clone(),
+            |mut oam| {
+                for i in 0..5_000_000 {
+                    oam.write((i % OAM_ENTRIES_COUNT * 4) as u16 + OAM_ADDR_START, (i & 0xFF) as u8);
                 }
             },
             BatchSize::SmallInput,
