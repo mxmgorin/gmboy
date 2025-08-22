773367f4a4b659656cd1caa9838bca64b22f003e
Troidem <onlinexmax@gmail.com>
2025-08-22 22:09:36 +0300

    refactor(cpu): add move cb instr to fns

--- core/src/cpu/instructions/misc/prefix.rs
+++ core/src/cpu/instructions/misc/prefix.rs
@@ -2,6 +2,147 @@ use crate::cpu::{Cpu, RegisterType};
 
 impl Cpu {
     #[inline(always)]
+    fn _execute_cb_bit_const<const B: u8, const R: u8>(&mut self) {
+        let val = self.get_register8::<R>();
+        self.registers.flags.set_z((val & (1 << B)) == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(true);
+    }
+
+    #[inline(always)]
+    fn execute_cb_bit(&mut self, register: &RegisterFn, bit: u16) {
+        let val = (register.get)(self);
+        self.registers.flags.set_z((val & (1 << bit)) == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(true);
+    }
+
+    #[inline(always)]
+    fn execute_cb_res(&mut self, register: &RegisterFn, bit: u16) {
+        let mut val = (register.get)(self);
+        val &= !(1 << bit);
+        (register.set)(self, val);
+    }
+
+    #[inline(always)]
+    fn execute_cb_set(&mut self, register: &RegisterFn, bit: u16) {
+        let mut reg_val = (register.get)(self);
+        reg_val |= 1 << bit;
+        (register.set)(self, reg_val);
+    }
+
+    #[inline(always)]
+    fn execute_cb_rlc(&mut self, register: &RegisterFn) {
+        let reg_val = (register.get)(self);
+        let carry = (reg_val & 0x80) != 0; // Check MSB for carry
+        let result = (reg_val << 1) | (carry as u8); // Rotate left and wrap MSB to LSB
+
+        (register.set)(self, result);
+        self.registers.flags.set_z(result == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c(carry);
+    }
+
+    #[inline(always)]
+    fn execute_cb_rrc(&mut self, register: &RegisterFn) {
+        let mut reg_val = (register.get)(self);
+        let old = reg_val;
+        reg_val = reg_val >> 1 | (old << 7);
+
+        (register.set)(self, reg_val);
+        self.registers.flags.set_z(reg_val == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c(old & 1 != 0);
+    }
+
+    #[inline(always)]
+    fn execute_cb_rl(&mut self, register: &RegisterFn) {
+        let mut reg_val = (register.get)(self);
+        let old = reg_val;
+        let flag_c = self.registers.flags.get_c();
+        reg_val = (reg_val << 1) | (flag_c as u8);
+
+        (register.set)(self, reg_val);
+
+        self.registers.flags.set_z(reg_val == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c((old & 0x80) != 0);
+    }
+
+    #[inline(always)]
+    fn execute_cb_rr(&mut self, register: &RegisterFn) {
+        let mut reg_val = (register.get)(self);
+        let old = reg_val;
+        let flag_c = self.registers.flags.get_c();
+        reg_val = (reg_val >> 1) | ((flag_c as u8) << 7);
+
+        (register.set)(self, reg_val);
+
+        self.registers.flags.set_z(reg_val == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c((old & 1) != 0);
+    }
+
+    #[inline(always)]
+    fn execute_cb_sla(&mut self, register: &RegisterFn) {
+        let mut reg_val = (register.get)(self);
+        let old = reg_val;
+        reg_val <<= 1;
+
+        (register.set)(self, reg_val);
+
+        self.registers.flags.set_z(reg_val == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c((old & 0x80) != 0);
+    }
+
+    #[inline(always)]
+    fn execute_cb_sra(&mut self, register: &RegisterFn) {
+        let reg_val = (register.get)(self);
+        let u: i8 = reg_val as i8;
+        let u = (u >> 1) as u8;
+        let result = (reg_val >> 1) | (reg_val & 0x80); // Shift right and preserve MSB
+        let carry = reg_val & 0x01 != 0; // Save LSB as Carry
+
+        (register.set)(self, result);
+
+        self.registers.flags.set_z(u == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c(carry);
+    }
+
+    #[inline(always)]
+    fn execute_cb_swap(&mut self, register: &RegisterFn) {
+        let mut reg_val = (register.get)(self);
+        reg_val = ((reg_val & 0xF0) >> 4) | ((reg_val & 0x0F) << 4);
+        (register.set)(self, reg_val);
+
+        self.registers.flags.set_z(reg_val == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c(false);
+    }
+
+    #[inline(always)]
+    fn execute_cb_srl(&mut self, register: &RegisterFn) {
+        let reg_val = (register.get)(self);
+        let u = reg_val >> 1;
+
+        (register.set)(self, u);
+
+        self.registers.flags.set_z(u == 0);
+        self.registers.flags.set_n(false);
+        self.registers.flags.set_h(false);
+        self.registers.flags.set_c((reg_val & 1) != 0);
+    }
+
+    #[inline(always)]
     pub fn fetch_execute_prefix(&mut self) {
         self.fetch_d8();
         let op = self.step_ctx.fetched_data.value;
@@ -14,127 +155,32 @@ impl Cpu {
         let register = REGISTER_FNS[register_index];
         let bit = (op >> 3) & 0b111;
         let bit_op = (op >> 6) & 0b11;
-        let mut reg_val = (register.get)(self);
 
         match bit_op {
             1 => {
-                // BIT
-                self.registers.flags.set_z((reg_val & (1 << bit)) == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(true);
+                self.execute_cb_bit(&register, bit);
                 return;
             }
             2 => {
-                // RST
-                reg_val &= !(1 << bit);
-                (register.set)(self, reg_val);
+                self.execute_cb_res(&register, bit);
                 return;
             }
             3 => {
-                // SET
-                reg_val |= 1 << bit;
-                (register.set)(self, reg_val);
+                self.execute_cb_set(&register, bit);
                 return;
             }
             _ => {}
         }
 
         match bit {
-            0 => {
-                // RLC
-                let carry = (reg_val & 0x80) != 0; // Check MSB for carry
-                let result = (reg_val << 1) | (carry as u8); // Rotate left and wrap MSB to LSB
-
-                (register.set)(self, result);
-                self.registers.flags.set_z(result == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c(carry);
-            }
-            1 => {
-                // RRC
-                let old = reg_val;
-                reg_val = reg_val >> 1 | (old << 7);
-
-                (register.set)(self, reg_val);
-                self.registers.flags.set_z(reg_val == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c(old & 1 != 0);
-            }
-            2 => {
-                // RL
-                let old = reg_val;
-                let flag_c = self.registers.flags.get_c();
-                reg_val = (reg_val << 1) | (flag_c as u8);
-
-                (register.set)(self, reg_val);
-
-                self.registers.flags.set_z(reg_val == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c((old & 0x80) != 0);
-            }
-            3 => {
-                // RR
-                let old = reg_val;
-                let flag_c = self.registers.flags.get_c();
-                reg_val = (reg_val >> 1) | ((flag_c as u8) << 7);
-
-                (register.set)(self, reg_val);
-
-                self.registers.flags.set_z(reg_val == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c((old & 1) != 0);
-            }
-            4 => {
-                // SLA
-                let old = reg_val;
-                reg_val <<= 1;
-
-                (register.set)(self, reg_val);
-
-                self.registers.flags.set_z(reg_val == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c((old & 0x80) != 0);
-            }
-            5 => {
-                // SRA
-                let u: i8 = reg_val as i8;
-                let u = (u >> 1) as u8;
-                let result = (reg_val >> 1) | (reg_val & 0x80); // Shift right and preserve MSB
-                let carry = reg_val & 0x01 != 0; // Save LSB as Carry
-
-                (register.set)(self, result);
-
-                self.registers.flags.set_z(u == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c(carry);
-            }
-            6 => {
-                // SWAP
-                reg_val = ((reg_val & 0xF0) >> 4) | ((reg_val & 0x0F) << 4);
-                (register.set)(self, reg_val);
-
-                self.registers.flags.set_z(reg_val == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c(false);
-            }
-            7 => {
-                // SRL
-                let u = reg_val >> 1;
-
-                (register.set)(self, u);
-
-                self.registers.flags.set_z(u == 0);
-                self.registers.flags.set_n(false);
-                self.registers.flags.set_h(false);
-                self.registers.flags.set_c((reg_val & 1) != 0);
-            }
+            0 => self.execute_cb_rlc(&register),
+            1 => self.execute_cb_rrc(&register),
+            2 => self.execute_cb_rl(&register),
+            3 => self.execute_cb_rr(&register),
+            4 => self.execute_cb_sla(&register),
+            5 => self.execute_cb_sra(&register),
+            6 => self.execute_cb_swap(&register),
+            7 => self.execute_cb_srl(&register),
             _ => {
                 panic!("ERROR: INVALID CB: {op:02X}");
             }
