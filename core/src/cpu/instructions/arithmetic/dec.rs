8e6730f0391d32b0b046d090909e00b7cdf2ff2b
Troidem <onlinexmax@gmail.com>
2025-08-23 15:34:47 +0300

    perf(cpu): remove branch for dec

--- core/src/cpu/instructions/arithmetic/dec.rs
+++ core/src/cpu/instructions/arithmetic/dec.rs
@@ -5,16 +5,14 @@ impl Cpu {
     pub fn fetch_execute_dec_r<const R1: u8>(&mut self) {
         self.fetch_r::<R1>();
         let mut value = self.step_ctx.fetched_data.value.wrapping_sub(1);
-        let r1 = RegisterType::from_u8(R1);
-
-        if r1.is_16bit() {
-            self.clock.tick_m_cycles(1);
-        }
-
         self.registers.set_register::<R1>(value);
         value = self.registers.read_register::<R1>();
 
-        self.set_flags_dec(value);
+        if RegisterType::from_u8(R1).is_16bit() {
+            self.clock.tick_m_cycles(1);
+        } else {
+            self.set_flags_dec(value);
+        }
     }
 
     #[inline(always)]
@@ -25,11 +23,8 @@ impl Cpu {
         self.set_flags_dec(value);
     }
 
+    #[inline(always)]
     fn set_flags_dec(&mut self, value: u16) {
-        if (self.step_ctx.opcode & 0x0B) == 0x0B {
-            return;
-        }
-
         self.registers.flags.set_z(value == 0);
         self.registers.flags.set_n(true);
         self.registers.flags.set_h((value & 0x0F) == 0x0F);
